470d686290b4b71a84e83685ff9311416148a91e
cathyleeu <leeyu823@gmail.com>
2016-09-07 01:13:01 +0900

    remove redux from post

--- src/createStore.js
+++ src/createStore.js
@@ -7,7 +7,7 @@ const middleware = applyMiddleware(thunk);
 export default (data = {}) => {
   const rootReducer = combineReducers({
     //every modules reducer should be define here
-    [feed.NAME]: feed.reducer,
+    // [feed.NAME]: feed.reducer,
     [nav.NAME]: nav.reducer,
     [tab.NAME]: tab.reducer,
     [profile.NAME]: profile.reducer
