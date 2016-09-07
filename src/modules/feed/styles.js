dc29efd7440f88f12fadcea10fa0bbab0e659c76
cathyleeu <leeyu823@gmail.com>
2016-09-07 10:40:03 +0900

    added nav header in feedhome

--- src/modules/feed/styles.js
+++ src/modules/feed/styles.js
@@ -1,9 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  navHeader: {
+		backgroundColor: '#5e5e5e',
+	},
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    height: 24,
+    width: 24,
+    margin: Platform.OS === 'ios' ? 10 : 16,
+  },
+  menu: {
+    height: 24,
+    width: 24,
+    margin: Platform.OS === 'ios' ? 10 : 16,
+    marginLeft: 15,
+  },
 })
