a573707678e8757ae3f472e5556fbfa29eed184a
cathyleeu <leeyu823@gmail.com>
2016-09-07 13:11:11 +0900

    계속 수정중...ㅠ

--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
 import React from 'react'
+import { Component } from 'react-native';
 import { Provider } from 'react-redux';
-import { feed } from './modules'
+
+import { tab } from './modules'
+
 import createStore from './createStore'
 
 const store = createStore()
 
-// const Enactus = () => {
-//   return (
-//     <Provider store={store}>
-//       <app.App />
-//     </Provider>
-//   )
-// }
-
-function Enactus() {
-  class Root extends React.Component {
-    render() {
-      return (
-        <Provider store={store}>
-          <feed.Feed />
-        </Provider>
-      )
-    }
-  }
-  return Root;
+const Enactus = () => {
+  return (
+    <Provider store={store}>
+      <tab.Tab />
+    </Provider>
+  )
 }
 
-// module.exports = Enactus;
 export default Enactus
 //<app.App />은 app/App.js를 실행 하는 것
