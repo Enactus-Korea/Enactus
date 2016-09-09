c6d7295c45e4f3ea2d52f084f046b75b0c14476f
cathyleeu <leeyu823@gmail.com>
2016-09-09 17:07:18 +0900

    delete

--- src/modules/root/Root.js
+++ src/modules/root/Root.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native';
 import React, { Component, PropTypes } from 'react'
 import { Global } from './components'
+import { connect } from 'react-redux'
+import * as actions from './actions'
 import Drawer from 'react-native-drawer'
-import Panel from '../panel/Panel'
-// import Control from './components/Control'
-
-
+// import Panel from '../panel/Panel'
+import Control from './components/Control'
 class Root extends Component {
   state={
 		drawerOpen: false,
@@ -17,23 +17,32 @@ class Root extends Component {
 	openDrawer = () => {
 		this._drawer.open()
 	};
+  // _changePanel (i) {
+  //   const { change } = this.props
+  //   change(i)
+  // }
+  // _renderPanelContent(key){
+  //   switch (key) {
+  //     //  case 'news':    return <Feed />
+  //      case 'intro':   return <Intro />
+  //      case 'network': return <Network />
+  //     //  case 'unknown': return <Unknown />
+  //     //  case 'archive': return <Archive />
+  //     //  case 'config':  return <Config />
+  //     //  case 'login':   return <Login />
+  //    }
+  // }
   render(){
     return (
       <Drawer
         ref={(ref) => this._drawer = ref}
         type="overlay"
         content={
-          <Panel closeDrawer={this.closeDrawer} />
-        }
+          <Control closeDrawer={this.closeDrawer}
+          /> }
         styles={{main: {shadowColor: '#000000', shadowOpacity: 0.3, shadowRadius: 15}}}
-        onOpen={() => {
-          console.log('onopen')
-          this.setState({drawerOpen: true})
-        }}
-        onClose={() => {
-          console.log('onclose')
-          this.setState({drawerOpen: false})
-        }}
+        onOpen={() => { this.setState({drawerOpen: true}) }}
+        onClose={() => { this.setState({drawerOpen: false}) }}
         tweenDuration={100}
         panThreshold={0.08}
         disabled={this.state.drawerDisabled}
@@ -46,4 +55,17 @@ class Root extends Component {
   }
 }
 // panOpenMask={0.2} => 추가하면 손으로 열기 가능..ㅎㅎㅎ
-export default Root
+// export default Root
+
+Root.displayName = 'Root'
+Root.propTypes = {
+  change: PropTypes.func.isRequired,
+}
+export default connect(
+  (state) => ({
+    panels: state.root
+  }),
+  (dispatch) => ({
+    change: (index) => dispatch(actions.change(index))
+  })
+)(Root)
--- src/modules/root/constants.js
+++ src/modules/root/constants.js
@@ -2,3 +2,4 @@ export const NAME = 'root'
 
 export const PUSH_ROUTE = `${NAME}/PUSH_ROUTE`
 export const POP_ROUTE = `${NAME}/POP_ROUTE`
+export const CHANGE_PANEL = `${NAME}/CHANGE_PANEL`
