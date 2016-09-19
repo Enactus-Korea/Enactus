8a7ca8b0c3367a8e25c56865292205e9528223e4
cathyleeu <leeyu823@gmail.com>
2016-09-19 16:06:37 +0900

    modified nav styles

--- app/components/global_widgets/nav.js
+++ app/components/global_widgets/nav.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import styles from './styles'
 
 export default class Nav extends Component {
 constructor(props){
@@ -15,7 +16,7 @@ menu(){
     this.props.onPress();
 
   } else if(this.props.state.navProps.type == "pop"){
-    this.props.state.navigator.push({id:'inbox'});
+    this.props.state.navigator.push({id:'feed'});
   }
 }
 
@@ -29,20 +30,22 @@ notification() {
 
   render() {
     return (
-      <View style= {{padding:20, backgroundColor:this.props.state.navStyle.backgroundColor, flexDirection:'row'}}>
-				<TouchableOpacity onPress ={() => this.menu()}>
-					<Icon color={this.props.state.navStyle.color} name={this.props.state.navProps.icon} size={24} />
+      <View style={styles.navContainer}>
+				<TouchableOpacity
+					style={styles.LeftComp}
+					onPress ={() => this.menu()}>
+					<Icon color='white' name={this.props.state.navProps.icon} size={24} />
 				</TouchableOpacity>
-        <Text style={{alignSelf:'center', marginLeft:10, fontWeight:'600', color:'#fff', fontSize:16}}>{this.props.state.navProps.name}</Text>
+        <Text style={styles.TitleComp}>{this.props.state.navProps.name}</Text>
 				{this.props.state.navProps.name === "인액터스" ?
-					<View style={{flexDirection: 'row'}}>
-						<TouchableOpacity onPress ={() => this.notification()}>
-							<Icon name="notifications" size={24}/>
-						</TouchableOpacity>
-						<TouchableOpacity onPress ={() => this.search()}>
-							<Icon name="search" size={24}/>
-						</TouchableOpacity>
-					</View> : <View />}
+				<View style={styles.RightComp}>
+					<TouchableOpacity onPress ={() => this.notification()}>
+						<Icon name="notifications" color='white' size={24} style={{paddingLeft:10}}/>
+					</TouchableOpacity>
+					<TouchableOpacity onPress ={() => this.search()}>
+						<Icon name="search" color='white' size={24} style={{paddingLeft:10}} />
+					</TouchableOpacity>
+				</View> : <View style={styles.RightComp} />}
       </View>
     );
   }
