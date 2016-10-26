c447dd6c6246f628ec8cd66eb389a5a9d918bd0b
cathyleeu <leeyu823@gmail.com>
2016-10-26 11:12:27 +0900

    react-native-camera 추가

--- app/components/post/post.js
+++ app/components/post/post.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Image,TextInput, Text, CameraRoll,View,TouchableOpacity,AlertIOS} from 'react-native';
+import { Image,TextInput, Text, CameraRoll,View,TouchableOpacity,AlertIOS, Modal,StatusBar} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import PostHead from './postHead'
 import styles from './styles'
+import Camera from 'react-native-camera'
 
 class Post extends Component{
   constructor(props){
@@ -11,7 +12,16 @@ class Post extends Component{
       userimg: 'Avatar',
       content: '',
       avatarSource: null,
-      modalVisible: true
+      modalVisible: true,
+      photoModalVisible: false,
+      camera: {
+        aspect: Camera.constants.Aspect.fill,
+        captureTarget: Camera.constants.CaptureTarget.cameraRoll,
+        type: Camera.constants.Type.back,
+        orientation: Camera.constants.Orientation.auto,
+        flashMode: Camera.constants.FlashMode.auto,
+      },
+      isRecording: false,
       // imageSource: '', =>  prop 이상하다고 오류 메시지 생김
     }
   }
@@ -51,6 +61,36 @@ class Post extends Component{
     this.props.setModalVisible(false);
     // TODO: refreshing 추가
   }
+  setModalVisible(visible) {
+    // post.js에서 param이 넘어올 때, bind(this)에 담겨서 넘어온다.
+    this.setState({photoModalVisible: visible});
+  }
+
+  renderModal() {
+    return(
+      <Modal
+        animationType={"slide"}
+        transparent={false}
+        visible={this.state.photoModalVisible}
+        onRequestClose={() => {alert("Modal has been closed.")}}
+        >
+        <StatusBar
+          animated
+          hidden
+        />
+        <Camera
+          ref={(cam) => {
+              this.camera = cam;
+            }}
+          {...this.props}
+          aspect={this.state.camera.aspect}
+          captureTarget={this.state.camera.captureTarget}
+          type={this.state.camera.type}
+          flashMode={this.state.camera.flashMode}
+          setModalVisible={this.setModalVisible.bind(this)}/>
+      </Modal>
+    )
+  }
   onPost(){
     return(
       <TouchableOpacity onPress={() => this.onPostPressed()}>
@@ -87,6 +127,7 @@ class Post extends Component{
         <View>
           <TouchableOpacity
             style={styles.avatarContainer}
+            
           >
             <View>
             { this.state.avatarSource === null ?
